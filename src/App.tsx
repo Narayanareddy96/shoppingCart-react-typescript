0085f1aaad5c2df69c570364d0b5bc2df9ea37e4
Venkat <gurrala.gvnreddy@gmail.com>
2023-04-26 20:03:42 -0500

    Added the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './components/Home';
+import { Store } from './components/Store';
+import { About } from './components/About';
 function App() {
-  return <>Hello</>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          Home
+        </Route>
+        <Route path="/store" element={<Store />}>
+          Store
+        </Route>
+        <Route path="/about" element={<About />}>
+          About
+        </Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
